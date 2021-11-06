132c997c2d85c492748aded852f65651c8872140
Victor <vicbem@hotmail.com>
2021-11-06 14:39:33 -0300

    Instrucao BEQAC adicionada

--- testbench_MIC_AMUX_ALU.vhd
+++ testbench_MIC_AMUX_ALU.vhd
@@ -761,8 +761,111 @@ TEST : PROCESS
 		Signal_A          <= "0000"; -- Endereco PC
 		Signal_MEM_TO_MBR <= "0000000000000000"; -- Nao importa
 		Signal_DATA_OK    <= '0'; -- Nao importa
+	
+ 		---------------------------------
+		wait for 40 ns;
+		-- Colocando possivel instrucao em IR
+		Signal_AMUX       <= '0';
+		Signal_ALU        <= "0010";
+		Signal_MBR        <= '1';
+		Signal_MAR        <= '0';
+		Signal_RD         <= '1';
+		Signal_WR         <= '0';
+		Signal_ENC        <= '0';
+		Signal_CTRLA      <= "00";
+		Signal_CTRLB      <= "00";
+		Signal_C          <= "0000";
+		Signal_B          <= "0000";
+		Signal_A          <= "0000";
+		Signal_MEM_TO_MBR <= "1111000110100010";
+		Signal_DATA_OK    <= '1';
 		
+		wait for 40 ns;
+
+		Signal_AMUX       <= '1';
+		Signal_ALU        <= "0010";
+		Signal_MBR        <= '0';
+		Signal_MAR        <= '0';
+		Signal_RD         <= '0';
+		Signal_WR         <= '0';
+		Signal_ENC        <= '1';
+		Signal_CTRLA      <= "00";
+		Signal_CTRLB      <= "00";
+		Signal_C          <= "0011";
+		Signal_B          <= "0000";
+		Signal_A          <= "0000";
+		Signal_MEM_TO_MBR <= "0000000000000000";
+		Signal_DATA_OK    <= '0';
 		
+		wait for 40 ns;
+
+		Signal_AMUX       <= '0';
+		Signal_ALU        <= "0010";
+		Signal_MBR        <= '1';
+		Signal_MAR        <= '0';
+		Signal_RD         <= '0';
+		Signal_WR         <= '0';
+		Signal_ENC        <= '0';
+		Signal_CTRLA      <= "00";
+		Signal_CTRLB      <= "00";
+		Signal_C          <= "0011";
+		Signal_B          <= "0000";
+		Signal_A          <= "0011";
+		Signal_MEM_TO_MBR <= "0000000000000000";
+		Signal_DATA_OK    <= '0';
+
+		--Execução BEQAC
+		wait for 40 ns;
+		--Ciclo 1: alu := RA - RB;
+		Signal_AMUX       <= '0'; -- Escolhe entrada A amux
+		Signal_ALU        <= "0110"; -- A - B
+		Signal_MBR        <= '0'; -- não escreve em MBR
+		Signal_MAR        <= '0'; -- Nao escreve em MAR
+		Signal_RD         <= '0'; -- Nao ler da memoria
+		Signal_WR         <= '0'; -- NAo escreve na memoria
+		Signal_ENC        <= '0'; -- nao escreve no banco
+		Signal_CTRLA      <= "01"; -- Endereco RA
+		Signal_CTRLB      <= "10"; -- Endereco RB
+		Signal_C          <= "0001"; -- nao importa
+		Signal_B          <= "0000"; -- nao importa
+		Signal_A          <= "0001"; -- nao importa
+		Signal_MEM_TO_MBR <= "0000000000000000"; -- Nao importa
+		Signal_DATA_OK    <= '0'; -- Nao importa
+
+		wait for 40 ns;
+		--Ciclo 2: PC := PC + AC;
+		Signal_AMUX       <= '0'; -- Escolhe entrada A amux
+		Signal_ALU        <= "0000"; -- soma
+		Signal_MBR        <= '0'; -- não escreve em MBR
+		Signal_MAR        <= '0'; -- Nao escreve em MAR
+		Signal_RD         <= '0'; -- Nao ler da memoria
+		Signal_WR         <= '0'; -- NAo escreve na memoria
+		Signal_ENC        <= '1'; -- escreve no banco
+		Signal_CTRLA      <= "00"; -- endereço A
+		Signal_CTRLB      <= "00"; -- endereço B
+		Signal_C          <= "0000"; -- endereço PC
+		Signal_B          <= "0000"; -- endereço PC
+		Signal_A          <= "0001"; -- endereço AC
+		Signal_MEM_TO_MBR <= "0000000000000000"; -- Nao importa
+		Signal_DATA_OK    <= '0'; -- Nao importa
+		
+		wait for 40 ns;
+		--Conferir dado no PC
+		Signal_AMUX       <= '0'; -- Escolhe entrada A amux
+		Signal_ALU        <= "0010"; -- Transparencia de A
+		Signal_MBR        <= '1'; -- escreve em MBR
+		Signal_MAR        <= '0'; -- Nao escreve em MAR
+		Signal_RD         <= '0'; -- Nao le da memoria
+		Signal_WR         <= '0'; -- NAo escreve na memoria
+		Signal_ENC        <= '0'; -- nao escreve no banco
+		Signal_CTRLA      <= "00"; -- Endereco A
+		Signal_CTRLB      <= "00"; -- nao importa
+		Signal_C          <= "0001"; -- nao importa
+		Signal_B          <= "0000"; -- nao importa
+		Signal_A          <= "0000"; -- Endereco PC
+		Signal_MEM_TO_MBR <= "0000000000000000"; -- Nao importa
+		Signal_DATA_OK    <= '0'; -- Nao importa
+
 		wait;
 END PROCESS TEST;
 
